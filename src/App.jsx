63feed3968237f34cc29ab0bcbe06581275c2755
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-12-02 16:46:37 +0100

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ import "./App.css";
 import languages from "./db/languages";
 
 function App() {
-  const [title, setTitle] = useState(languages[0].title);
-  const [description, setDescription] = useState(languages[0].description);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState(
+    "Nessun lunguaggio selezionato"
+  );
+  const [colorBtn, setColorBtn] = useState("");
+
+  const handleColorBtn = (index) => {
+    languages[index - 1].isActive = !languages[index - 1].isActive;
+    languages[index - 1].isActive == true
+      ? setColorBtn("active")
+      : setColorBtn("");
+    // console.log(languages[index - 1].isActive);
+  };
 
   return (
     <>
@@ -14,61 +25,61 @@ function App() {
         {/* button section */}
         <div className="button-container">
           <button
-            className={"button"}
-            isDisplayed={true}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[0].title);
               setDescription(languages[0].description);
+              handleColorBtn(languages[0].id);
             }}
           >
             HTML
           </button>
           <button
-            className={"button"}
-            isDisplayed={false}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[1].title);
               setDescription(languages[1].description);
+              handleColorBtn(languages[1].id);
             }}
           >
             CSS
           </button>
           <button
-            className={"button"}
-            isDisplayed={false}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[2].title);
               setDescription(languages[2].description);
+              handleColorBtn(languages[2].id);
             }}
           >
             JavaScript
           </button>
           <button
-            className={"button"}
-            isDisplayed={false}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[3].title);
               setDescription(languages[3].description);
+              handleColorBtn(languages[3].id);
             }}
           >
             Node.js
           </button>
           <button
-            className={"button"}
-            isDisplayed={false}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[4].title);
               setDescription(languages[4].description);
+              handleColorBtn(languages[4].id);
             }}
           >
             Express
           </button>
           <button
-            className={"button"}
-            isDisplayed={false}
+            className={`button  + ${colorBtn}`}
             onClick={() => {
               setTitle(languages[5].title);
               setDescription(languages[5].description);
+              handleColorBtn(languages[5].id);
             }}
           >
             ReactJS
